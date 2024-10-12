ffa616bb39c6d2e69fbb1b26c2c9a92a3cb58fee
Mudassir Nawaz Javaid <mudassirnawazjavaid@gmail.com>
2024-10-12 20:06:25 +0500

    dark mode: fixed, purgecss stopped from base

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,18 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./hugo_stats.json"],
-  defaultExtractor: (content) => {
-    const els = JSON.parse(content).htmlElements;
-    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
-  },
-  safelist: [],
-});
+// const purgecss = require("@fullhuman/postcss-purgecss")({
+//   content: ["./hugo_stats.json"],
+//   defaultExtractor: (content) => {
+//     const els = JSON.parse(content).htmlElements;
+//     return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+//   },
+//   safelist: [],
+// });
 
 const config = {
   plugins: [
     require("autoprefixer"),
     require("tailwindcss"),
     require("postcss-import"),
-    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
+    // ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
   ],
 };
 
